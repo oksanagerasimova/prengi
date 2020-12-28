8c0c63188aef67e5b8aeec98c1c075bb684c36b6
oksana <oksanastyles69@gmail.com>
2020-12-28 15:43:08 +0300

    finale

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,12 @@
 $(document).ready(function(){
     $('.expert__carousel').slick({
+        dots: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png"></button>',
         
         responsive: [
             {
-              breakpoint: 768,
-              settings: {
-                dots: true,
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-              }
-            },
-            {
-              breakpoint: 480,
+              breakpoint: 770,
               settings: {
                 dots: true,
                 arrows: false,
@@ -27,3 +18,21 @@ $(document).ready(function(){
           ]
     });
   });
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu__list'),
+    menuItem = document.querySelectorAll('.menu__list-item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu__list_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu__list_active');
+        })
+    })
+  })
